132e8675c1b021cb41f8ca6c91b0187748dfe0e4
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-10 15:07:14 -0500

    Create index.tsx

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 50 }}>
+      <h1>Bienvenido al Sistema de Planilla</h1>
+      <p>Login pendiente de implementar</p>
+    </div>
+  );
+}
