041ee948662699a490d75be521add5e77721fa15
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-11 18:12:16 -0500

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { Module, Controller, Get } from '@nestjs/common';
-
-@Controller()
-class AppController {
-  @Get()
-  getRoot(): string {
-    return 'API OK';
-  }
-}
-
-@Module({
-  controllers: [AppController],
-})
-class AppModule {}
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
