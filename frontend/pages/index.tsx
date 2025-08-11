a2f685c7591e69601f5b2e8c6219c835ccb8be9f
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-11 12:13:29 -0500

    Delete frontend/pages directory

--- frontend/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: 50 }}>
-      <h1>Frontend OK</h1>
-      <p>Conexión lista para consumir el backend</p>
-    </div>
-  );
-}
