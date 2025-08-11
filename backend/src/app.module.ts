891803e5ddf5abd2f9eadf05aeef8cb71580bf0c
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-11 18:08:59 -0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PingController } from './ping.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PingController } from './ping.controller';
 
 @Module({
   imports: [],
