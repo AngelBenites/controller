8d6d85a7c85a5b2c72d5580b61cfe3563ebbecf7
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 11:06:59 -0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { EmpleadosModule } from './empleados/empleados.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingController } from './ping.controller';
 
 @Module({
-  imports: [AuthModule,],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',        // Ajusta aquí
+      port: 5432,
+      username: 'tu_usuario',
+      password: 'tu_contraseña',
+      database: 'tu_base_de_datos',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,        // solo para desarrollo
+    }),
+    AuthModule,
+    EmpleadosModule,
+  ],
   controllers: [
     AppController,
-    PingController // 👈 lo registras aquí
+    PingController,
   ],
   providers: [AppService],
 })
