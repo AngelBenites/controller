867410554456d5a93fd63a678550d51f83ed8c2e
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 11:05:31 -0500

    Update empleados.module.ts

--- backend/src/empleados/empleados.module.ts
+++ backend/src/empleados/empleados.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Empleado } from './entities/empleado.entity';
+import { EmpleadosService } from './empleados.service';
+import { EmpleadosController } from './empleados.controller';
 
+@Module({
+  imports: [TypeOrmModule.forFeature([Empleado])],
+  providers: [EmpleadosService],
+  controllers: [EmpleadosController],
+})
+export class EmpleadosModule {}
