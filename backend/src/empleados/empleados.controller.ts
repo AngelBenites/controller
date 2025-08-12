fb310b06d53f907bc6aa957b35c72b76ab10123a
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 11:00:57 -0500

    Update empleados.controller.ts

--- backend/src/empleados/empleados.controller.ts
+++ backend/src/empleados/empleados.controller.ts
@@ -1,1 +1,33 @@
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { EmpleadosService } from './empleados.service';
+import { Empleado } from './entities/empleado.entity';
 
+@Controller('empleados')
+export class EmpleadosController {
+  constructor(private readonly empleadosService: EmpleadosService) {}
+
+  @Get()
+  findAll(): Promise<Empleado[]> {
+    return this.empleadosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Empleado> {
+    return this.empleadosService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() empleadoData: Partial<Empleado>): Promise<Empleado> {
+    return this.empleadosService.create(empleadoData);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateData: Partial<Empleado>): Promise<Empleado> {
+    return this.empleadosService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<void> {
+    return this.empleadosService.remove(id);
+  }
+}
