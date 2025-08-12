ea26675626c2df18ff229d0d0dc98588efb541e5
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 14:41:22 -0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { EmpleadosModule } from './empleados/empleados.module';
+import { PlanillasModule } from './planillas/planillas.module'; // <-- Importa el módulo planillas
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PingController } from './ping.controller';
@@ -10,16 +12,18 @@ import { PingController } from './ping.controller';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',        // Ajusta aquí
+      host: 'localhost',        
       port: 5432,
       username: 'tu_usuario',
       password: 'tu_contraseña',
       database: 'tu_base_de_datos',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,        // solo para desarrollo
+      synchronize: true,
     }),
     AuthModule,
     EmpleadosModule,
+    PlanillasModule,   // <-- Agrega aquí el módulo planillas
+    // FinanzasModule  // <-- Luego agrega este cuando tengas listo ese módulo
   ],
   controllers: [
     AppController,
