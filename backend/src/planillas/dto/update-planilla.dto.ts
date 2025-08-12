2cce4ebc5cc4efbfef936e6695822e8217e3eed9
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 14:39:48 -0500

    Update update-planilla.dto.ts

--- backend/src/planillas/dto/update-planilla.dto.ts
+++ backend/src/planillas/dto/update-planilla.dto.ts
@@ -1,1 +1,5 @@
 
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePlanillaDto } from './create-planilla.dto';
+
+export class UpdatePlanillaDto extends PartialType(CreatePlanillaDto) {}
