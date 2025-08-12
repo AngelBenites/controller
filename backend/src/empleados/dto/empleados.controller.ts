aadef7dc6a382caef0c2fac96d8dbd8165919993
HWPeru <33811126+AngelBenites@users.noreply.github.com>
2025-08-12 11:02:09 -0500

    Create empleados.controller.ts

---
+++ backend/src/empleados/dto/empleados.controller.ts
@@ -0,0 +1,3 @@
+import { CreateEmpleadoDto } from './dto/create-empleado.dto';
+import { UpdateEmpleadoDto } from './dto/update-empleado.dto';
+import { UsePipes, ValidationPipe } from '@nestjs/common';
